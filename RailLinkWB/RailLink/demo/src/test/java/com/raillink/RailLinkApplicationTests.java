6b5ca241316e1c0d91c1c21837093a2a1d5e0868
IT24102348 <kpjmp28@gmail.com>
2025-09-24 10:19:59 +0530

    f

---
+++ RailLinkWB/RailLink/demo/src/test/java/com/raillink/RailLinkApplicationTests.java
@@ -0,0 +1,15 @@
+package com.raillink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("mysql")
+class RailLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+} 
